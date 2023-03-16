c0d7b60720c11e1af69a711c13bfd3bbe981c62e
Mohamed Said <me@msaied.com>
2023-03-16 14:43:02 +0200

    Install Laravel & Base Models Structure

---
+++ app/Http/Controllers/IngredientsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+
+class IngredientsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Ingredient $ingredient)
+    {
+    }
+
+    public function edit(Ingredient $ingredient)
+    {
+    }
+
+    public function update(Request $request,Ingredient $ingredient)
+    {
+    }
+
+    public function destroy(Ingredient $ingredient)
+    {
+    }
+}
---
+++ database/migrations/2023_03_16_142055_create_ingredients_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('ingredients',function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->integer('stock_grams');
+            $table->boolean('alert_sent')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
